e18a3c429384c1b1d6c6f99069eb05f2e5f5f4fa
“Pratik” <pratikbhandurge2222@gmail.com>
2022-09-16 20:36:15 +0530

    CabInvoiceGeneratorSolutionAdded

---
+++ CabInvoiceGeneratorTest/InvoiceGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CabInvoiceGeneratorTest
+{
+    internal class InvoiceGenerator
+    {
+        public InvoiceGenerator(object pREMIUM)
+        {
+        }
+
+        internal double CalculateFare(double distance, int time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
